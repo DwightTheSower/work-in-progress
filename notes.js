f218e8e4360fbcad4c6cf7fe64318199cb011337
Dwight Houweling <cdhouweling@hotmail.com>
2020-03-21 13:54:57 +0200

    Added server.js file to backend so that we can run "nodemon server.js"

---
+++ notes.js
@@ -0,0 +1,14 @@
+1. Go to the folder when you want to create the react project and type
+"npx create-react-app <name-of-folder>"
+
+2. Go into folder and type "npm start" to show browser based development server
+
+3. Inside <name-of-folder> create folder called "backend"
+
+4. Inside backend folder, type "npm init -y" to create a packages json file
+
+5. Inside backend folder, install dependencies: "npm install express cors mongo$
+
+6. Inside backend folder, create "server.js"
+
+7.     
